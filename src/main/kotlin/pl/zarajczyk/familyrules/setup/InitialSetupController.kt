5be0787ebc0922762627cb40fc6a0c5227d46498
rzarajczyk <rzarajczyk@gmail.com>
2024-07-30 20:21:06 +0200

    unregister-instance

--- src/main/kotlin/pl/zarajczyk/familyrules/setup/InitialSetupController.kt
+++ src/main/kotlin/pl/zarajczyk/familyrules/setup/InitialSetupController.kt
@@ -9,34 +9,58 @@ import pl.zarajczyk.familyrules.shared.*
 @RestController
 class InitialSetupController(private val dbConnector: DbConnector) {
 
-    @PostMapping("/setup")
-    fun setup(
-        @RequestBody data: InitialSetupRequest,
-        @RequestHeader("Authorization", required = false) authHeader: String?
-    ): InitialSetupResponse = try {
+    @PostMapping("/register-instance")
+    fun registerInstance(
+        @RequestBody data: RegisterInstanceRequest,
+        @RequestHeader("Authorization") authHeader: String
+    ): RegisterInstanceResponse = try {
         val auth = authHeader.decodeBasicAuth()
         dbConnector.validatePassword(auth.user, auth.pass)
         val token = dbConnector.setupNewInstance(auth.user, data.instanceName)
-        InitialSetupResponse(InitialSetupStatus.SUCCESS, token)
+        RegisterInstanceResponse(RegisterInstanceStatus.SUCCESS, token)
     } catch (e: InvalidPassword) {
-        InitialSetupResponse(InitialSetupStatus.INVALID_PASSWORD)
+        RegisterInstanceResponse(RegisterInstanceStatus.INVALID_PASSWORD)
     } catch (e: InstanceAlreadyExists) {
-        InitialSetupResponse(InitialSetupStatus.INSTANCE_ALREADY_EXISTS)
+        RegisterInstanceResponse(RegisterInstanceStatus.INSTANCE_ALREADY_EXISTS)
     } catch (e: IllegalInstanceName) {
-        InitialSetupResponse(InitialSetupStatus.ILLEGAL_INSTANCE_NAME)
+        RegisterInstanceResponse(RegisterInstanceStatus.ILLEGAL_INSTANCE_NAME)
+    }
+
+    @PostMapping("/unregister-instance")
+    fun unregisterInstance(
+        @RequestBody data: UnregisterInstanceRequest,
+        @RequestHeader("Authorization") authHeader: String
+    ): UnregisterInstanceResponse = try {
+        val auth = authHeader.decodeBasicAuth()
+        dbConnector.validatePassword(auth.user, auth.pass)
+        UnregisterInstanceResponse(UnregisterInstanceStatus.SUCCESS)
+    } catch (e: InvalidPassword) {
+        UnregisterInstanceResponse(UnregisterInstanceStatus.INVALID_PASSWORD)
     }
 
 }
 
-data class InitialSetupRequest(
+data class RegisterInstanceRequest(
     val instanceName: String
 )
 
-enum class InitialSetupStatus {
+enum class RegisterInstanceStatus {
     SUCCESS, INSTANCE_ALREADY_EXISTS, ILLEGAL_INSTANCE_NAME, INVALID_PASSWORD
 }
 
-data class InitialSetupResponse(
-    val status: InitialSetupStatus,
+data class RegisterInstanceResponse(
+    val status: RegisterInstanceStatus,
     val token: String? = null
+)
+
+data class UnregisterInstanceRequest(
+    val instanceName: String
+)
+
+enum class UnregisterInstanceStatus {
+    SUCCESS, INVALID_PASSWORD
+}
+
+data class UnregisterInstanceResponse(
+    val status: UnregisterInstanceStatus
 )
