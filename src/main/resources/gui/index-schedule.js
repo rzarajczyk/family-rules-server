d16ff21448ec94859c33e032f5cad2f9b1f7fed9
rzarajczyk <rzarajczyk@gmail.com>
2024-09-28 16:02:33 +0200

    schedule

--- src/main/resources/gui/index-schedule.js
+++ src/main/resources/gui/index-schedule.js
@@ -29,19 +29,21 @@ function renderTimetable(tpl, content) {
     content.innerHTML = tpl(templateData)
     content.querySelectorAll(".schedule-cell").forEach(it => {
         it.addEventListener('mouseover', (e) => {
-//            e.target.style.backgroundColor = 'var(--md-ref-palette-tertiary90)'
             e.target.classList.add('add-period')
         })
         it.addEventListener('mouseout', (e) => {
-//            e.target.style.backgroundColor = ''
             e.target.classList.remove('add-period')
         })
         it.addEventListener('click', (e) => {
             let day = e.target.dataset.day
             let time = { hour: e.target.dataset.hour, minute: e.target.dataset.minute }
             document.querySelector('#add-period-modal-start').value = formatTime(time)
+            document.querySelector('#add-period-modal-start').disabled = false
             document.querySelector('#add-period-modal-end').value = formatTime({hour: parseInt(time.hour) + 1, minute: time.minute})
+            document.querySelector('#add-period-modal-end').disabled = false
             document.querySelector('#add-period-modal-day').selectedIndex = templateData.days.indexOf(day)
+            document.querySelector('#add-period-modal-day').disabled = false
+            document.querySelector('#add-period-modal-ok').innerHTML = 'Add period'
             M.FormSelect.init(document.querySelector('#add-period-modal-day'), {})
             M.Modal.getInstance(document.querySelector('#add-period-modal')).open()
         })
@@ -81,6 +83,21 @@ function renderSchedule(data, content) {
                 div.title = `${period.state.title}\n${formatTime(period.from)}-${formatTime(period.to)}`
                 div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="var(--md-sys-color-on-background)" style="width: 100%; max-width: 48px; height: 100%; max-height: 48px;">${period.state.icon}</svg>`
                 div.classList.add('period')
+
+                div.addEventListener('click', () => {
+                    document.querySelector('#add-period-modal-start').value = formatTime(period.from)
+                    document.querySelector('#add-period-modal-start').disabled = true
+                    document.querySelector('#add-period-modal-end').value = formatTime(period.to)
+                    document.querySelector('#add-period-modal-end').disabled = true
+                    let index = Array.from(document.querySelectorAll('#add-period-modal-day option'))
+                        .map(it => it.value.toUpperCase())
+                        .indexOf(day)
+                    document.querySelector('#add-period-modal-day').disabled = true
+                    document.querySelector('#add-period-modal-day').selectedIndex = index
+                    M.FormSelect.init(document.querySelector('#add-period-modal-day'), {})
+                    document.querySelector('#add-period-modal-ok').innerHTML = 'Update'
+                    M.Modal.getInstance(document.querySelector('#add-period-modal')).open()
+                })
 
                 content.appendChild(div)
             }
