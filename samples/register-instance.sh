065628c703107ccd3b908e23fddea834ae313508
rzarajczyk <rzarajczyk@gmail.com>
2025-09-28 21:15:22 +0200

    small edit

--- samples/register-instance.sh
+++ samples/register-instance.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 HOST="http://localhost:8080"
-INSTANCE_NAME="sony-tv"
-CLIENT_TYPE="ANDROID_TV"
-USER=""
-PASS=""
+INSTANCE_NAME="test-instance"
+CLIENT_TYPE="TEST"
+USER="admin"
+PASS="admin"
 
 curl --header "Content-Type: application/json" \
   --request POST \
   --user "$USER:$PASS" \
   --data "{\"instanceName\":\"$INSTANCE_NAME\",\"clientType\":\"$CLIENT_TYPE\"}" \
-  "$HOST/api/v1/register-instance"
+  "$HOST/api/v2/register-instance"
--- samples/report.sh
+++ samples/report.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 HOST="http://localhost:8080"
-INSTANCE_ID=""
+INSTANCE_ID="c7aaca32-988c-455e-8a39-89668d79628"
 USER="admin"
-TOKEN=""
+TOKEN="f6dbd5e1-c8f6-4d57-b8ac-5ce1d81110ff"
 
 curl --header "Content-Type: application/json" \
   --request POST \
   --verbose \
   --user "$USER:$TOKEN" \
-  --data "{\"instanceId\":\"$INSTANCE_ID\",\"screenTime\":0, \"applications\": {}}" \
-  "$HOST/api/v1/report"
+  --data "{\"instanceId\":\"$INSTANCE_ID\",\"screenTime\":10, \"applications\": {}}" \
+  "$HOST/api/v2/report"
