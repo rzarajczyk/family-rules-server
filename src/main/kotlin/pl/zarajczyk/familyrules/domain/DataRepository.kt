bbc0bbebf1da171bdb383e4b6ca33022fc87e7af
rzarajczyk <rzarajczyk@gmail.com>
2025-11-08 23:15:57 +0100

    UsersService.kt

--- src/main/kotlin/pl/zarajczyk/familyrules/domain/DataRepository.kt
+++ src/main/kotlin/pl/zarajczyk/familyrules/domain/DataRepository.kt
@@ -2,16 +2,6 @@ package pl.zarajczyk.familyrules.domain
 
 import kotlinx.datetime.LocalDate
 
-interface UsersRepository {
-    // User operations
-    fun findUser(username: String): UserDto?
-    fun validatePassword(username: String, password: String)
-    fun changePassword(username: String, newPassword: String)
-    fun getAllUsers(): List<UserDto>
-    fun deleteUser(username: String)
-    fun createUser(username: String, password: String, accessLevel: AccessLevel)
-}
-
 interface DataRepository {
     fun findInstance(id: InstanceId): InstanceRef?
     fun findInstances(username: String): List<InstanceRef>
